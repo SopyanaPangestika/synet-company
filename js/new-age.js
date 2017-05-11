acbacbd107f53e2d78a2f964fe723048946f1ab2
putra rolli <sofyanapangestika@gmail.com>
2017-05-11 18:58:07 +0700

    new slider text design

--- js/new-age.js
+++ js/new-age.js
@@ -27,9 +27,7 @@
             top: 50
         }
     })
-	$('#slides').superslides({
-      animation: 'fade'
-    });
+	
 	$('#layananproduk').DataTable();
 		//google map
 		var map;
@@ -47,6 +45,20 @@
 		  }
 
 		  google.maps.event.addDomListener(window, 'load', initialize);
+	var headertext = [],
+		headers = document.querySelectorAll("#miyazaki th"),
+		tablerows = document.querySelectorAll("#miyazaki th"),
+		tablebody = document.querySelector("#miyazaki tbody");
+
+		for(var i = 0; i < headers.length; i++) {
+		  var current = headers[i];
+		  headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+		} 
+		for (var i = 0, row; row = tablebody.rows[i]; i++) {
+		  for (var j = 0, col; col = row.cells[j]; j++) {
+			col.setAttribute("data-th", headertext[j]);
+		  } 
+		}
 
 
 })(jQuery); // End of use strict
